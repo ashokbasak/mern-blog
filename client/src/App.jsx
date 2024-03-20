641c024578e0fc369dd92d9a52a62b0376cebc49
ashokbasak <ashokbasak80@gmail.com>
2024-03-20 19:04:38 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import Home from "./pages/Home";
+import News from "./pages/News";
+
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/news" element={<News />} />
+          {/* Define other routes that you need*/}
+        </Routes>
+      </main>
+    </Router>
+  );
+};
+
+export default App;
