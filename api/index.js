6913a2ce80be2efc97e57f680be830ba2eef031b
ashokbasak <ashokbasak80@gmail.com>
2024-03-24 17:53:44 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
 import express from "express";
-const PORT = 4000;
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+const PORT = process.env.PORT || 4000;
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("Connected to the database"))
+  .catch((err) => console.log(err));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT} `);
 });
+
+// userName:ashokbasak80
+// password:ashok1980
+//mongodb+srv://ashokbasak80:ashok1980@cluster0.33swabv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//mongodb+srv://ashokbasak80:<password>@cluster0.33swabv.mongodb.net/
